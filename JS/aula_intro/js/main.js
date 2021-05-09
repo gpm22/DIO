71b266e8e3deab53c4ac4ddf7c9d1c20df27e1c1
gpm22 <gabrielpacmil@gmail.com>
2021-05-09 00:04:26 -0300

    mandando tudo pra outra pasta

--- JS/aula_intro/js/main.js
+++
@@ -1,6 +0,0 @@
-function botao() {
-  //alert('clica mais, safado!!!!!!!!!!1')
-  document.getElementById('pica').innerHTML = 'rolão';
-  console.log(document.getElementById('pica'))
-  console.log(document.getElementById('pica').innerHTML)
-}
